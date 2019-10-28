a4a484aa24214e7b1d9792597efe848d8ed15beb
mnash29 <mtntrades10@gmail.com>
2019-10-28 16:08:43 -0700

    Added binary proof of work system

--- cryptochain/average-work.js
+++ cryptochain/average-work.js
@@ -20,5 +20,5 @@ for (let i = 0; i<10000; i++) {
 
     average = times.reduce((total, num) => (total + num))/times.length;
 
-    console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms.`)
+    console.log(`${i+1}) Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms.`)
 }
