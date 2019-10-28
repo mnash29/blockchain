7b2d14f45cdbd95d472fcc34bf16b06fac7665e8
mnash29 <mtntrades10@gmail.com>
2019-10-28 15:35:32 -0700

    Added mining difficulty adjustment

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,4 +1,4 @@
-const { GENESIS_DATA } = require('./config');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
@@ -19,16 +19,25 @@ class Block {
 		let hash, timestamp;
 		//const timestamp = Date.now();
 		const lastHash = lastBlock.hash;
-		const { difficulty } = lastBlock;
+		let { difficulty } = lastBlock;
 		let nonce = 0;
 
 		do {
 			nonce++;
 			timestamp = Date.now();
+			difficulty = Block.adjustDifficulty({ originalBlock: lastBlock, timestamp });
 			hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
 		} while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
 		return new this({timestamp, lastHash, data, difficulty, nonce, hash});
+	}
+
+	static adjustDifficulty({ originalBlock, timestamp }) {
+		const { difficulty } = originalBlock;
+
+		if ((timestamp - originalBlock.timestamp) > MINE_RATE ) return difficulty - 1;
+
+		return difficulty + 1;
 	}
 }
 
